e9fcfd194859882da2ebeed5c718f011ff0736e1
joel-baby <joelbaby005@gmail.com>
2026-06-04 19:53:25 +0530

    day 6

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:client/pages/login_page.dart';
 import 'package:client/widget/drop_down_btn.dart';
+import 'package:client/widget/multi_select_dropdown.dart';
+import 'package:client/widget/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -40,14 +42,38 @@ class HomePage extends StatelessWidget {
           ),
           Row(
             children: [
-              DropDownBtn(
-                items: ['Rs Low to High', 'Rs High to Low'],
-                selectedItemText: 'Sort items',
-                onSelected: (selected) {},
+              Flexible(
+                child: DropDownBtn(
+                  items: ['Rs Low to High', 'Rs High to Low'],
+                  selectedItemText: 'Sort items',
+                  onSelected: (selected) {},
+                ),
+              ),
+              Flexible(
+                child: MultiSelectDropdown(items: ['item1', 'item2', 'item3']),
               ),
             ],
           ),
-
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.8,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ProductCard(
+                  name: 'Puma footwear',
+                  imageUrl:
+                      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                  price: 200,
+                  offerTag: '30% off',
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
